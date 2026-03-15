6a4a9ba6cffb0db9d88ad5b500a4670e3b6649bf
Habib Mohammadi <hamed.nova@gmail.com>
2026-03-15 12:24:22 +0100

    add domain models for Bitcoin price and error types

---
+++ BitconPrices/Repository/Models/BitcoinPrice.swift
@@ -0,0 +1,39 @@
+//
+//  BitcoinPrice.swift
+//  BitconPrices
+//
+
+import Foundation
+
+/// The price of Bitcoin in all three supported fiat currencies for a single point in time.
+struct BitcoinPrice: Equatable {
+    let eur: Double
+}
+
+/// The price entry for a single calendar day.
+struct DailyPrice: Identifiable, Equatable {
+    let date: Date
+    let eurPrice: Double
+
+    var id: Date { date }
+}
+
+/// All errors that can be produced by the network / repository layers.
+enum BitcoinPriceError: Error, Equatable {
+    case invalidURL
+    case networkError(URLError)
+    case httpError(statusCode: Int)
+    case decodingError
+    case unknown
+
+    static func == (lhs: BitcoinPriceError, rhs: BitcoinPriceError) -> Bool {
+        return switch (lhs, rhs) {
+        case (.invalidURL, .invalidURL): true
+        case (.networkError(let a), .networkError(let b)): a.code == b.code
+        case (.httpError(let a), .httpError(let b)): a == b
+        case (.decodingError, .decodingError): true
+        case (.unknown, .unknown): true
+        default: false
+        }
+    }
+}
